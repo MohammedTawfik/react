495406f47ab82947653e029fccb2cd61ffce63bd
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2024-01-25 13:53:59 +0200

    Configuring basic routing for public and private routes

--- snapgram/src/app.tsx
+++ snapgram/src/app.tsx
@@ -1,11 +1,25 @@
 import { Route, Routes } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/Rootlayout";
 
 const app = () => {
   return (
     <main className="flex h-screen">
-      {/* Public Routes (any one can access) */}
+      <Routes>
+        {/* Public Routes (any one can access) */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-      {/* Private Routes (only register users can access) */}
+        {/* Private Routes (only registered users can access) */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </main>
   );
 };
