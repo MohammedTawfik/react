b2e798f19aec2d37c90ce19c8af3f2ebd844c760
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2024-01-26 11:45:00 +0200

    Adding assests files

--- snapgram/src/_auth/AuthLayout.tsx
+++ snapgram/src/_auth/AuthLayout.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <Navigate to="/" />
+        </div>
+      ) : (
+        <>
+          <section className="flex flex-col justify-center items-center flex-1 py-10">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default AuthLayout;
